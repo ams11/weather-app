abe73a4f18354257ab44e5fc4001b1c435988209
Alex Slepak <ams11@github.com>
2023-01-02 23:11:37 -0800

    add a cached indicator

--- app/helpers/forecasts_helper.rb
+++ app/helpers/forecasts_helper.rb
@@ -1,7 +1,9 @@
 module ForecastsHelper
+  include ActionView::Helpers::DateHelper
+
   def forecast_info_from_forecast_data(forecast_data)
     {
-      cached: !!forecast_data["cached"],
+      cached: !!forecast_data[:cached] ? time_ago_in_words(forecast_data[:cached]) : nil,
       location: forecast_data["name"],
       temperature: forecast_data["main"]["temp"],
       conditions: forecast_data["weather"].first["main"],
